147924f0fd5577bb7de405eb603759ae60f1b32f
jahenderson <jahenderson@csumb.edu>
2018-02-07 03:12:08 +0000

    added assignments 1, 2, removed hello world

---
+++ Assignments/1/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Assignment 1</title>
+    </head>
+    <body>
+        This is Assignment 1.
+        <img src="imageShare.img.jpg" />
+        <div>
+            <a href="/">Back</a>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -5,8 +5,19 @@
     </head>
     <body>
         <div>
+            <h1>Assignments</h1>
+            <!--/ means "start at root" which is git-demo folder-->
+            <div>
+                <a href="/Assignments/1">Assignment 1</a>
+            </div>
+            <div>
+                <a href="/Assignments/2">Assignment 2</a>
+            </div>
+            <!--no / means "start here" which is still git-demo folder-->
             <h1>Labs</h1>
-            <a href="/Labs/1">Lab 1</a>
+            <div>
+                <a href="Labs/1">Lab 1</a>
+            </div>
         </div>
     </body>
 </html>
